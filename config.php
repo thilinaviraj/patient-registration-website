360fc53022f11671c9b3bf2e7548108c0362d776
Adniwhack <rasnayake.kavinda@gmail.com>
2016-02-16 19:53:52 +0530

    Added Config File

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Contains MySQL Database Configuration*/
+
+define('DB_HOST', 'localhost'); //Host machine address
+define('DB_USER', 'root'); //Username
+define('DB_PASS', ''); //Password
+define('DB_DATA', 'patientregistration'); //Database
+
+?>
+
