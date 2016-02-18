dcbc348331e8ed37d13a3d72394f6cc9b3a767bd
thilinaviraj <thilinaviraj950@gmail.com>
2016-02-18 22:04:06 +0530

    Validations
    I've added the validations for the input fields

--- addDetails.php
+++ addDetails.php
@@ -20,9 +20,7 @@
         $res = $log->patient_insert($FirstName, $LastName, $Address, $City, $Telephone, $NIC, $Disability, $Reason, $Description, $latitude, $longitude);
         if (!$res){
             //Show error modal
-        }
-        
-        
+        }        
     }
 ?>
 
@@ -36,12 +34,13 @@
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"> </script>
-        <style>
+            <!-- Adding recaptcha file in to the page -->
+            <style>
             body{
-                background: #8999A8;
+                background:#ffe6e6;
             }
             .navbar, .dropdown-menu{
-            background:rgba(255,255,255,0.25);
+            background:rgba(255, 128, 128,0.25);
             border: none;
 
             }
@@ -54,7 +53,7 @@
               background: none;
             }
             .navbar a, .dropdown-menu>li>a, .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover, .navbar-toggle{
-             color: #fff;
+             color: #000066;
             }
             .dropdown-menu{
                   -webkit-box-shadow: none;
@@ -96,8 +95,6 @@
     </head>
     
     <body>
-    
-        
             <div class="container-fluid">
                 <nav class="navbar navbar-fixed">
                     <div class="navbar-header">
@@ -121,99 +118,214 @@
         
         <div class="container">
             <link rel="stylesheet" href="formoid_files/formoid1/formoid-solid-blue.css" type="text/css" />
-            <script type="text/javascript" src="formoid_files/formoid1/jquery.min.js"></script>
             <script type="text/javascript">
-        var oldMarker;
-
-        var map;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 7.873054, lng: 80.771797},
-                zoom:8
-            });
-            var infoWindow = new google.maps.InfoWindow();
-            var latlngbounds = new google.maps.LatLngBounds();
-
-            var geocoder = new google.maps.Geocoder();
-           
-            document.getElementById('city').addEventListener('keyup', function() {
-                geocodeAddress(geocoder, map);
-            })
-
-            google.maps.event.addListener(map,'click', function(e){
-                document.getElementById("latitude").value = e.latLng.lat();
-                document.getElementById("longitude").value = e.latLng.lng();
-                var lat = e.latLng.lat();
-                var lng = e.latLng.lng();
-                placeMarker(e.latLng);
-
-            })
-
-
-
-        }
-        function geocodeAddress(geocoder, resultsMap) {
-            var address = document.getElementById('city').value;
-            try {
-                geocoder.geocode({'address': address, 'region': 'lk'}, function (results, status) {
-                    if (status === google.maps.GeocoderStatus.OK) {
-                        resultsMap.setCenter(results[0].geometry.location);
-
-                    } else {
-                        //alert('Geocode was not successful for the following reason: ' + status);
-                    }
+            var oldMarker;
+            var map;
+            function initMap() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 7.873054, lng: 80.771797},
+                    zoom:8
                 });
+                var infoWindow = new google.maps.InfoWindow();
+                var latlngbounds = new google.maps.LatLngBounds();
+                var geocoder = new google.maps.Geocoder();
+                document.getElementById('city').addEventListener('keyup', function() {
+                    geocodeAddress(geocoder, map);
+                })
 
+                google.maps.event.addListener(map,'click', function(e){
+                    document.getElementById("latitude").value = e.latLng.lat();
+                    document.getElementById("longitude").value = e.latLng.lng();
+                    var lat = e.latLng.lat();
+                    var lng = e.latLng.lng();
+                    placeMarker(e.latLng);
+                })
             }
-            catch(err){}
-        }
-        function placeMarker(location) {
+            function geocodeAddress(geocoder, resultsMap) {
+                var address = document.getElementById('city').value;
+                try {
+                    geocoder.geocode({'address': address, 'region': 'lk'}, function (results, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            resultsMap.setCenter(results[0].geometry.location);
 
-
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map
-            });
-
-
-            if (oldMarker != undefined){
-                oldMarker.setMap(null);
+                        } else {
+                            //alert('Geocode was not successful for the following reason: ' + status);
+                        }
+                    });
+                }
+                catch(err){}
             }
-            oldMarker = marker;
-
-        }
-
-    </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsgBFqz2cjiLyvYBDRHrOdgf9vDBIvno8&callback=initMap">
-    </script>
-            <form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:600px;min-width:150px" method="post"><div class="title"><h2>Registration form</h2></div>
-                <div class="element-input" title="First Name"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="First Name"/><span class="icon-place"></span></div></div>
-                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input1" placeholder="Last Name"/><span class="icon-place"></span></div></div>
-                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" placeholder="NIC"/><span class="icon-place"></span></div></div>
+            function placeMarker(location) {
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                });
+                if (oldMarker != undefined){
+                    oldMarker.setMap(null);
+                }
+                oldMarker = marker;
+            }
+            </script>
+            <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsgBFqz2cjiLyvYBDRHrOdgf9vDBIvno8&callback=initMap">
+            </script>
+    
+            <form class="formoid-solid-blue" id="contactForm" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:600px;min-width:150px" method="post"><div class="title"><h2>Registration form</h2></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="First Name" /><span class="icon-place"></span></div></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input1" placeholder="Last Name" /><span class="icon-place"></span></div></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" maxlength="10" placeholder="NIC" /><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input3" placeholder="Address"/><span class="icon-place"></span></div></div>
-                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="city" id="city" placeholder="City "/><span class="icon-place"></span></div></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="city" id="city" placeholder="City"/><span class="icon-place"></span></div></div>
                 <div id="map" style="width: 550px; height: 300px"></div>
-                
-                <input  id="latitude" name="latitude" type="hidden">
-                <input  id="longitude" name="longitude" type="hidden">
-               
-                <div class="element-phone"><label class="title"></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" placeholder="Contact Number" value=""/><span class="icon-place"></span></div></div>
+                <input id="latitude" name="latitude" type="hidden">
+                <input id="longitude" name="longitude" type="hidden">
+                <div class="element-phone"><label class="title"></label><div class="item-cont"><input class="large" type="tel" pattern="^\d{10}$" maxlength="10" name="phone" placeholder="Contact Number" value=""/><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input5" placeholder="Disability"/><span class="icon-place"></span></div></div>
                 <div class="element-textarea"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" placeholder="Reason"></textarea><span class="icon-place"></span></div></div>
                 <div class="element-textarea"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea1" cols="20" rows="5" placeholder="Description"></textarea><span class="icon-place"></span></div></div>
-                
-            
-    
-        
-        <div class="submit"><input type="submit" value="Submit"/></div>
-           
-            
+                <div class="form-group">
+                    <div class="col-md-9 col-md-offset-3">
+                        <div id="messages"></div>
+                    </div>                                    
+                </div>
+                <div class="submit"><input type="submit" value="Submit"/></div>
             </form>
-            
-            <p class="frmd"><a href="http://formoid.com/v29.php">online form</a> Formoid.com 2.9</p><script type="text/javascript" src="formoid_files/formoid1/formoid-solid-blue.js"></script>
-            <!-- Stop Formoid form-->
+            <p class="frmd"><a href="http://formoid.com/v29.php">online form</a> Formoid.com 2.9</p>
         </div>
         
+        <script type="text/javascript">
+	$(document).ready(function(){
+		$("#myModal").modal('show');
+	});
+        </script>
+        <div id="myModal" class="modal fade" >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" style="background-color:#FFFFFF;font-size:25px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:600px;min-width:150px">Confirmation</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to save the data?</p>
+                        <p class="text-warning"><small>If you don't save, your data will be lost.</small></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+   			
+        <script type="text/javascript">
+        $(document).ready(function() {
+            $('#contactForm').bootstrapValidator({
+                container: '#messages',
+                feedbackIcons: {
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
+                },
+                fields:{
+                    input:{
+                        validators:{
+                            notEmpty:{ 
+                                message: 'The first name cannot be empty'
+                            },  
+                            regexp:{
+                                regexp: /^[a-zA-Z]+$/,
+                                message: 'The first name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    input1: {
+                        validators: {
+                            notEmpty: {
+                                message: 'The last name cannot be empty'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z]+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    input2: {
+                        validators: {
+                            notEmpty: {
+                                message: 'The last name cannot be empty'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z0-9]+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    input3: {
+                        validators: {
+                            notEmpty: {
+                                message: 'The address cannot be empty'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z0-9_\.,' ']+$/,
+                                message: 'The address can only consist of alphabetical, number and underscore'
+                            }
+                        }
+                    },
+                    city: {
+                        validators: {
+                            notEmpty: { 
+                                message: 'Please select a city'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z]+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    phone: {
+                        validators: {
+                            notEmpty: { 
+                                message: 'Please select a city'
+                            }
+                            
+                        }
+                    },
+                    input5: {
+                        validators: {
+                            notEmpty: { 
+                                message: 'Please select a city'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z0-9_\.,' ']+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    textarea: {
+                        validators: {
+                            notEmpty: { 
+                                message: 'Please select a city'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z0-9_\.,' ']+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    },
+                    textarea1: {
+                        validators: {
+                            isEmpty: { 
+                                message: 'Please select a city'
+                            },
+                            regexp: {
+                                regexp: /^[a-zA-Z0-9_\.,' ']+$/,
+                                message: 'The last name can only consist of alphabetic values'
+                            }
+                        }
+                    }
+                }
+            });
+        });
+        </script>
     </body>
 </html>
