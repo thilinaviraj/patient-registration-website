28e5401338b81450b79036ac9b6729adc38a3c29
thilinaviraj <thilinaviraj950@gmail.com>
2016-02-20 16:36:35 +0530

    Adding data
    Here's the finalized files for the entering data

--- addDetails.php
+++ addDetails.php
@@ -27,6 +27,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Add Details</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>  
@@ -34,87 +35,19 @@
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"> </script>
-            <!-- Adding recaptcha file in to the page -->
-            <style>
-            body{
-                background:#ffe6e6;
-            }
-            .navbar, .dropdown-menu{
-            background:rgba(255, 128, 128,0.25);
-            border: none;
-
-            }
-
-            .nav>li>a, .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover, .dropdown-menu>li>a, .dropdown-menu>li{
-              border-bottom: 3px solid transparent;
-            }
-            .nav>li>a:focus, .nav>li>a:hover,.nav .open>a, .nav .open>a:focus, .nav .open>a:hover, .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover{
-              border-bottom: 3px solid transparent;
-              background: none;
-            }
-            .navbar a, .dropdown-menu>li>a, .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover, .navbar-toggle{
-             color: #000066;
-            }
-            .dropdown-menu{
-                  -webkit-box-shadow: none;
-                box-shadow:none;
-            }
-
-            .nav li:hover:nth-child(8n+1), .nav li.active:nth-child(8n+1){
-              border-bottom: #C4E17F 3px solid;
-            }
-            .nav li:hover:nth-child(8n+2), .nav li.active:nth-child(8n+2){
-              border-bottom: #F7FDCA 3px solid;
-            }
-            .nav li:hover:nth-child(8n+3), .nav li.active:nth-child(8n+3){
-              border-bottom: #FECF71 3px solid;
-            }
-            .nav li:hover:nth-child(8n+4), .nav li.active:nth-child(8n+4){
-              border-bottom: #F0776C 3px solid;
-            }
-            .nav li:hover:nth-child(8n+5), .nav li.active:nth-child(8n+5){
-              border-bottom: #DB9DBE 3px solid;
-            }
-            .nav li:hover:nth-child(8n+6), .nav li.active:nth-child(8n+6){
-              border-bottom: #C49CDE 3px solid;
-            }
-            .nav li:hover:nth-child(8n+7), .nav li.active:nth-child(8n+7){
-              border-bottom: #669AE1 3px solid;
-            }
-            .nav li:hover:nth-child(8n+8), .nav li.active:nth-child(8n+8){
-              border-bottom: #62C2E4 3px solid;
-            }
-
-            .navbar-toggle .icon-bar{
-                color: #fff;
-                background: #fff;
-            }
-        </style>
-        
-        
     </head>
     
     <body>
-            <div class="container-fluid">
-                <nav class="navbar navbar-fixed">
-                    <div class="navbar-header">
-      			<ul class="nav navbar-nav navbar-left"><li><img src="image/logo.jpg" height=50 width=250 align="left"></li></ul>
-                    </div>
-                        <div class="container">
-                            <div id="navbar" class="navbar-collapse collapse">
-                                <ul class="nav navbar-nav">
-                                    <li class=""><a href="#" class=""><span class="glyphicon glyphicon-saved"></span> Add details</a></li>
-                                    <li class=""><a href="#" class=""><span class="glyphicon glyphicon-search"></span> Search details</a></li>
-                                    <li><a href="#"><span class="glyphicon glyphicon-import"></span> Update details</a></li>
-                                    <li class=""><a href="#"><span class="glyphicon glyphicon-remove"></span> Delete details</a></li>
-                                </ul>
-                                <ul class="nav navbar-nav pull-right">
-                                    <li class=""><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                </nav>
-            </div>
+        
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+            <ul class="nav navbar-nav navbar-left"><li><img src="image/logo.jpg" height=50 width=250 align="left"></li></ul>
+       </div>
+        <ul class="nav navbar-nav pull-right">
+            <li><a href="search.php" style="font-size:17px;font-family:'Roboto';color:#34495E" class="dropdown-toggle"><span class="glyphicon glyphicon-search"></span> Search</a></li>
+            <li><a style="font-size:17px;font-family:'Roboto';color:#34495E" href="login.php" class="dropdown-toggle"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>                  
+        </ul>
+    </nav>
         
         <div class="container">
             <link rel="stylesheet" href="formoid_files/formoid1/formoid-solid-blue.css" type="text/css" />
@@ -173,31 +106,25 @@
             <form class="formoid-solid-blue" id="contactForm" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:600px;min-width:150px" method="post"><div class="title"><h2>Registration form</h2></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="First Name" /><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input1" placeholder="Last Name" /><span class="icon-place"></span></div></div>
-                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" maxlength="10" placeholder="NIC" /><span class="icon-place"></span></div></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" maxlength="10" minlength="10" placeholder="NIC"/><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input3" placeholder="Address"/><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="city" id="city" placeholder="City"/><span class="icon-place"></span></div></div>
+                <div class="element-input"><label class="title"></label><div class="item-cont"><h>Please select location from the map</h></div></div>
                 <div id="map" style="width: 550px; height: 300px"></div>
-                <input id="latitude" name="latitude" type="hidden">
-                <input id="longitude" name="longitude" type="hidden">
+                <input id="latitude" name="latitude" type="hidden" required>
+                <input id="longitude" name="longitude" type="hidden" required>
                 <div class="element-phone"><label class="title"></label><div class="item-cont"><input class="large" type="tel" pattern="^\d{10}$" maxlength="10" name="phone" placeholder="Contact Number" value=""/><span class="icon-place"></span></div></div>
                 <div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input5" placeholder="Disability"/><span class="icon-place"></span></div></div>
                 <div class="element-textarea"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" placeholder="Reason"></textarea><span class="icon-place"></span></div></div>
                 <div class="element-textarea"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea1" cols="20" rows="5" placeholder="Description"></textarea><span class="icon-place"></span></div></div>
-                <div class="form-group">
-                    <div class="col-md-9 col-md-offset-3">
-                        <div id="messages"></div>
-                    </div>                                    
+                <div class="form-group"><div class="col-md-9 col-md-offset-3"><div id="messages"></div></div>                                    
                 </div>
-                <div class="submit"><input type="submit" value="Submit"/></div>
+                <div class="submit"><input type="submit" id="submit" value="Submit"/></div>
             </form>
             <p class="frmd"><a href="http://formoid.com/v29.php">online form</a> Formoid.com 2.9</p>
         </div>
         
-        <script type="text/javascript">
-	$(document).ready(function(){
-		$("#myModal").modal('show');
-	});
-        </script>
+        
         <div id="myModal" class="modal fade" >
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -210,13 +137,17 @@
                         <p class="text-warning"><small>If you don't save, your data will be lost.</small></p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Save </button>
                     </div>
                 </div>
             </div>
         </div>
-   			
+   		<script type="text/javascript">
+	$(document).ready(function(){
+		$("#myModal").modal('show');
+	});
+         
+        </script>	
         <script type="text/javascript">
         $(document).ready(function() {
             $('#contactForm').bootstrapValidator({
