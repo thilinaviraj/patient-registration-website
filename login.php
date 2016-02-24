d5ad1efe6e24c644db7b95fb0df56e2562d3be12
thilinaviraj <thilinaviraj950@gmail.com>
2016-02-24 19:18:25 +0530

    Admin login
    I've changed the css

--- login.php
+++ login.php
@@ -18,12 +18,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8" />
+        <title>Login</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>  
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"> </script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="http://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+        <script src="js/vendor/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/vendor/jquery.sortelements.js" type="text/javascript"></script>
+        <script src="js/jquery.bdt.js" type="text/javascript"></script>
     <style>
         body{
-            background: #8999A8;
+            //background: #8999A8;
             }
     </style>
     <style>
@@ -38,7 +51,12 @@
     </style>
 </head>
 <body class="blurBg-false">
-    
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+            <ul class="nav navbar-nav navbar-left"><li><img src="image/logo.jpg" height=50 width=250 align="left"></li></ul>
+       </div>
+        
+    </nav>
     <!-- Start login form-->
     <link rel="stylesheet" href="login_files/formoid1/formoid-solid-blue.css" type="text/css" />
     <script type="text/javascript" src="login_files/formoid1/jquery.min.js"></script>
